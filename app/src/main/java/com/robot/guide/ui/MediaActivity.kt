b9d915a71b05bdab3cefe5c28ff09c5c3582b211
gzjwzh-ui <gzjwzh-ui@users.noreply.github.com>
2026-09-10 17:37:36 +0800

    fix: add missing Toast import in MediaActivity.kt + fix workflow pipefail

--- app/src/main/java/com/robot/guide/ui/MediaActivity.kt
+++ app/src/main/java/com/robot/guide/ui/MediaActivity.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
